3b36f3658fafa07fd043d584d6fe97814f5fb201
jaysonzxk <1344173512@qq.com>
2023-12-27 11:07:02 +0700

    ok

--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,7 @@ function resolve(dir) {
 }
 
 const name = defaultSettings.title || process.env.VUE_APP_TITLE || '管理系统' // 标题
-const port = process.env.port || process.env.npm_config_port || 80 // 端口
+const port = process.env.port || process.env.npm_config_port || 8001 // 端口
 
 // vue.config.js 配置说明
 //官方vue.config.js 参考文档 https://cli.vuejs.org/zh/config/#css-loaderoptions
@@ -22,7 +22,8 @@ module.exports = {
   // 用于放置生成的静态资源 (js、css、img、fonts) 的；（项目打包之后，静态资源会放在这个文件夹下）
   assetsDir: 'static',
   // 是否开启eslint保存检测，有效值：ture | false | 'error'
-  lintOnSave: process.env.NODE_ENV === 'development',
+  // lintOnSave: process.env.NODE_ENV === 'development',
+  lintOnSave: false,
   // 如果你不需要生产环境的 source map，可以将其设置为 false 以加速生产环境构建。
   productionSourceMap: false,
   // webpack-dev-server 相关配置
